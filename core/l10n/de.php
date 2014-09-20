2fb7a0f446955678051d0f052c7320bd1670547c
Volkan Gezer <volkangezer@gmail.com>
2014-09-20 15:27:56 +0200

    master init

---
+++ core/l10n/de.php
@@ -0,0 +1,24 @@
+<?php
+$TRANSLATIONS = array(
+"Saved" => "Gespeichert",
+"Email sent" => "Email gesendet",
+"Encryption" => "Verschlüsselung",
+"Unable to load list from App Store" => "Die Liste der Anwendungen im Store konnte nicht geladen werden.",
+"Authentication error" => "Authentifizierungs-Fehler",
+"Group already exists" => "Die Gruppe existiert bereits",
+"Unable to add group" => "Die Gruppe konnte nicht angelegt werden",
+"Email saved" => "E-Mail-Adresse gespeichert",
+"Invalid email" => "Ungültige E-Mail-Adresse",
+"Unable to delete group" => "Die Gruppe konnte nicht gelöscht werden",
+"Unable to delete user" => "Der Benutzer konnte nicht gelöscht werden",
+"Language changed" => "Sprache geändert",
+"Invalid request" => "Ungültige Anforderung",
+"Admins can't remove themself from the admin group" => "Administratoren können sich nicht selbst aus der admin-Gruppe löschen",
+"Unable to add user to group %s" => "Der Benutzer konnte nicht zur Gruppe %s hinzugefügt werden",
+"Unable to remove user from group %s" => "Der Benutzer konnte nicht aus der Gruppe %s entfernt werden",
+"Couldn't update app." => "Die App konnte nicht aktualisiert werden.",
+"User Documentation" => "Dokumentation für Benutzer",
+"Update to {appversion}" => "Update zu {appversion}",
+"Disable" => "Deaktivieren"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ core/l10n/tr.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"a" => "b",
+"cat" => "kedi",
+"dog" => "köpek"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
