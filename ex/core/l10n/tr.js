79fc1e4b501c0331b5b7b5d1c5ee824dd5d7f526
Volkan Gezer <volkangezer@gmail.com>
2015-02-15 16:44:44 +0100

    Add also four spaces before the string

--- ex/core/l10n/tr.js
+++ ex/core/l10n/tr.js
@@ -3,6 +3,6 @@ OC.L10N.register(
     {
     "To avoid timeouts with larger installations, you can instead run the following command from your installation directory:" : "Daha büyük kurulumlarda zaman aşımlarının önüne geçmek için, kurulum dizininizden aşağıdaki komutu da çalıştırabilirsiniz:",
     "This %s instance is currently being updated, which may take a while." : "Bu %s örneği şu anda güncelleniyor, bu biraz zaman alabilir.",
-    "This page will refresh itself when the %s instance is available again." : "Bu sayfa, %s örneği tekrar kullanılabilir olduğunda kendini yenileyecektir.",
+    "This page will refresh itself when the %s instance is available again." : "Bu sayfa, %s örneği tekrar kullanılabilir olduğunda kendini yenileyecektir."
 },
 "nplurals=2; plural=(n > 1);");
